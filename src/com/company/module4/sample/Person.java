d4080e4064d781bd80f90277313c79c33015d1b3
flackx <brendons.liflands@va.lv>
2022-11-23 09:42:09 +0200

    Added module4

---
+++ src/com/company/module4/sample/Person.java
@@ -0,0 +1,25 @@
+package com.company.module4.sample;
+
+public class Person {
+
+	//Attributes
+	private String name;
+	
+	
+	//Behavior
+	public Person(String name){
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String introduce(){
+		return "My name is " + name;
+	}
+	
+}
