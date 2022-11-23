8f366141ca599ef579d99b3e28b11e116293947c
flackx <brendons.liflands@va.lv>
2022-11-23 16:49:14 +0200

    added functions to the car task

--- src/tasksday3/bmw.java
+++ src/tasksday3/bmw.java
@@ -1,15 +1,16 @@
 package tasksday3;
 
-public class bmw extends Car{
+public class bmw extends Car {
     public static void main(String[] args) {
         bmw e46 = new bmw();
         e46.setName("BMW E46");
         e46.setWheels(4);
         e46.setDoors(4);
         e46.setEngineSize(3.0);
-        e46.setMaxSpeed(250);
+        e46.setSpeed(50);
         e46.setGears(6);
         e46.setGearbox("Manual");
+        e46.setGearUsed();
         System.out.println(e46);
 
 
